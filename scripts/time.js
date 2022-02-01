011ecede979644ba270db7b2bd20c76da6d0268e
LorenzoLolo <lorenzo.lopez-uroz@ensg.eu>
2022-02-01 15:30:56 +0100

    version alternative en commentaire si index spatiaux

--- scripts/time.js
+++ scripts/time.js
@@ -1,3 +1,7 @@
+function get_time_classes(""){
+
+}
+
 function define_temporal_where(time_classes){
   var list_where = [];
   for (const [class_number, class_dict] of time_classes){
@@ -5,7 +9,12 @@ function define_temporal_where(time_classes){
     where  = "WHERE date_part("+class_dict.field+", to_timestamp(startsecond));
     where += "BETWEEN "+str(class_dict.start)+" AND "+str(class_dict.end);
 
-    list_where.push(where)
+    /*
+    version alternative :
+    where = "WHERE "+str(class_dict.field)+" = "+str(class_dict.value)+"";
+    */
+
+    list_where.push(where);
   }
   return list_where;
 }
